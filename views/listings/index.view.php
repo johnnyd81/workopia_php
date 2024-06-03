cf0ef69589c55a789da913d1348a6ec7bd0782ff
Jonathan Johnson <95863021+johnnyd81@users.noreply.github.com>
2024-06-03 20:45:05 +0200

    Update index.view.php

--- views/listings/index.view.php
+++ views/listings/index.view.php
@@ -2,7 +2,7 @@
 <?= loadPartial('navbar'); ?>
 <?= loadPartial('top-banner') ?>
 
-<!-- Job Listings -->
+<!-- Job Listings page containing available jobs -->
 <section>
     <div class="container mx-auto p-4 mt-4">
         <div class="text-center text-3xl mb-4 font-bold border border-gray-300 p-3">Recent Jobs</div>
