b25a5a6d427bcfd82b7fc1f539ce2b9a6ee71638
Jonathan Johnson <95863021+johnnyd81@users.noreply.github.com>
2024-06-14 11:16:26 +0200

    Update home.php

--- controllers/home.php
+++ controllers/home.php
@@ -2,6 +2,7 @@
 //import db.php file that has data to connect to the workopia database
 $config = require basePath('config/db.php');
 
+//create a new database instance
 $db = new Database($config);
 
 $listings = $db->query('SELECT * FROM listings LIMIT 6')->fetchAll();
