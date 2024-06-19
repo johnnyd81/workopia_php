352f7b2adccb288bb2313fbbcf2a69f4425eaa3c
Jonathan Johnson <95863021+johnnyd81@users.noreply.github.com>
2024-06-19 10:11:27 +0200

    Update routes.php

--- routes.php
+++ routes.php
@@ -1,5 +1,5 @@
 <?php
-
+//the uri ('/') and the method ('get') decides which controller to be used i.e. which file to render in the browser
 $router->get('/', 'controllers/home.php');
 $router->get('/listings', 'controllers/listings/index.php');
 $router->get('/listings/create', 'controllers/listings/create.php');
