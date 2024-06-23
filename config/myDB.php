bbb3f0081034f158cc0ad06c069e90f0ace24075
Jonathan Johnson <95863021+johnnyd81@users.noreply.github.com>
2024-06-23 12:23:39 +0200

    Update myDB.php

--- config/myDB.php
+++ config/myDB.php
@@ -1,6 +1,7 @@
 <?php
 
 //associative array that contains the data to connect to the mysql database called workopia
+//there is no password as this is just a practice project 
 return [
     'host' => 'localhost',
     'port' => 3306,
