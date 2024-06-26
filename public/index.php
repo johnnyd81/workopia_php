6e3c6943b200261762ff35449f5df7d473e6d7d4
Jonathan Johnson <95863021+johnnyd81@users.noreply.github.com>
2024-06-26 10:41:40 +0200

    Update index.php

--- public/index.php
+++ public/index.php
@@ -1,5 +1,5 @@
 <?php
-// helpers.php contains functions that perform repetetive tasks
+// helpers.php contains functions that perform repetetive tasks i.e. displaying variables etc.
 require '../helpers.php';
 require basePath('Router.php');
 require basePath('Database.php');
