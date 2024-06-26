f7f9f3e3a8fcc961086304768c0c6bffbc7c87c7
Jonathan Johnson <95863021+johnnyd81@users.noreply.github.com>
2024-06-26 10:39:51 +0200

    Update myDB.php

--- config/myDB.php
+++ config/myDB.php
@@ -1,6 +1,7 @@
 <?php
 
 //associative array that contains the data to connect to the mysql database called workopia
+//the username and password are needed as well as a $dsn string to connect to the database
 //there is no password as this is just a practice project 
 return [
     'host' => 'localhost',
