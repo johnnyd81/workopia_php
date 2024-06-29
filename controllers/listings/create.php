850a71aca0dcdc9a4c77c56726f217981243bfae
Jonathan Johnson <95863021+johnnyd81@users.noreply.github.com>
2024-06-29 10:42:14 +0200

    Update create.php

--- controllers/listings/create.php
+++ controllers/listings/create.php
@@ -1,4 +1,4 @@
 <?php
 
-//loads the create listing form page that can add a new listing
+//loads the create listing form page that can add a new listing to the database
 loadView('/listings/create');
