929c7ac13392de6567e79e8c8096efe18b9ba30d
Jonathan Johnson <95863021+johnnyd81@users.noreply.github.com>
2024-06-29 10:41:30 +0200

    Update index.php

--- public/index.php
+++ public/index.php
@@ -10,7 +10,7 @@ $router = new Router();
 //get the routes from routes.php
 $routes = require basePath('routes.php');
 
-//get current URI and HTTP method
+//get current URI and HTTP method to use in the router
 $uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
 $method = $_SERVER['REQUEST_METHOD'];
 
