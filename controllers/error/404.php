b1d7bd2d7ad5dbc3bf632628ded5ea9118650cb3
Jonathan Johnson <95863021+johnnyd81@users.noreply.github.com>
2024-09-01 13:12:11 +0200

    Update 404.php

--- controllers/error/404.php
+++ controllers/error/404.php
@@ -1,5 +1,5 @@
 <?php
 
-//loads the error page using the loadView helper method located in the root of the application
+//loads the error page using the loadView helper method located in the root
 //the loadView function takes a view path as the only argument i.e. folder and file name
 loadView('error/404');
