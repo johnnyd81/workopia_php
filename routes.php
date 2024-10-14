fa8478eb3cf38f1e2c068f2391b94907e2e09e34
Jonathan Johnson <95863021+johnnyd81@users.noreply.github.com>
2024-10-14 16:46:48 +0200

    Update routes.php

--- routes.php
+++ routes.php
@@ -1,5 +1,5 @@
 <?php
-//each route is an instance of the Router class and accesses a specific method i.e. get
+//each route is an instance of the Router class and accesses a specific method i.e. get, post, put and delete
 $router->get('/', 'controllers/home.php');
 $router->get('/listings', 'controllers/listings/index.php');
 $router->get('/listings/create', 'controllers/listings/create.php');
