b8fa9bfa1510a33a5d60978c111a08f9d95935b8
Jonathan Johnson <95863021+johnnyd81@users.noreply.github.com>
2024-10-21 11:03:43 +0200

    Update Database.php

--- Database.php
+++ Database.php
@@ -27,6 +27,7 @@ class Database
             $this->conn = new PDO($dsn, $config['username'], $config['password'], $options);
         } catch (PDOException $e) {
             // handles an exception if an error occurs while connecting to the database
+            //the method $e->getMessage() returns the error message
             throw new Exception("Database connection failed: {$e->getMessage()}");
         }
     }
