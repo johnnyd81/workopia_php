3d5a569b48f5427d9e70444ab0ee6701d5f986c7
Jonathan Johnson <95863021+johnnyd81@users.noreply.github.com>
2024-12-14 21:01:24 +0200

    Update head.php

--- views/partials/head.php
+++ views/partials/head.php
@@ -1,6 +1,7 @@
 <!DOCTYPE html>
 <html lang="en">
 
+    <!--Head element containing meta tags, css link tags etc.-->
 <head>
     <meta charset="UTF-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
