f5c710111e8e0aa304913868b95218ba48da9dc8
Jonathan Johnson <95863021+johnnyd81@users.noreply.github.com>
2025-01-07 10:49:30 +0200

    Update home.php

--- controllers/home.php
+++ controllers/home.php
@@ -9,7 +9,7 @@ $db = new Database($config);
 //get the listings in the database but limit them to 6 results at a time
 $listings = $db->query('SELECT * FROM listings LIMIT 6')->fetchAll();
 
-//loads the homepage using the loadView helper method. The listings are passed in as the second argument
+//loads the homepage using the loadView helper method. The available listings that were retrieved are passed in as the second argument
 loadView('home', [
     'listings' => $listings
 ]);
