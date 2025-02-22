b728776197102f88a552105af86d3e1848f20fd2
Jonathan Johnson <95863021+johnnyd81@users.noreply.github.com>
2025-02-22 19:51:05 +0200

    Update show.php

--- controllers/listings/show.php
+++ controllers/listings/show.php
@@ -4,7 +4,7 @@
 $config = require basePath("config/myDB.php");
 $db = new Database($config);
 
-//get the id from the url via the $_GET superglobal
+//get the id from the url by using the $_GET superglobal
 $id = $_GET['id'] ?? '';
 
 //makes it safer to prevent unexpected sql injections in the database eg. accidentally dropping a table
