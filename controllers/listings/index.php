2dc4291b0d854dd9461653385c8da1178262ce8e
Jonathan Johnson <95863021+johnnyd81@users.noreply.github.com>
2025-02-22 19:50:28 +0200

    Update index.php

--- controllers/listings/index.php
+++ controllers/listings/index.php
@@ -4,7 +4,7 @@
 $config = require basePath("config/myDB.php");
 $db = new Database($config);
 
-//store the available listings in the listings variable but limit them to 6 listings at a time
+//store the available listings in the listings variable but limit them to 6 listings
 $listings = $db->query('SELECT * FROM listings LIMIT 6')->fetchAll();
 
 //pass the retrieved listings into the view to be displayed
