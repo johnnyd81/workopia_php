fdb04ddeeb16afd5678316ec4ee9ac850e4c5abe
Jonathan Johnson <95863021+johnnyd81@users.noreply.github.com>
2025-04-19 20:59:12 +0200

    Update 404.php

--- controllers/error/404.php
+++ controllers/error/404.php
@@ -1,4 +1,4 @@
 <?php
 
-//loads the error page using the loadView helper method found in the helpers file
+//loads the 404 error page using the loadView helper method found in the helpers file
 loadView('error/404');
