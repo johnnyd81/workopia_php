82de2b580fbc0e24f2c8dd10e78298d9b2fab725
Jonathan Johnson <95863021+johnnyd81@users.noreply.github.com>
2025-04-19 21:01:42 +0200

    Update show.php

--- controllers/listings/show.php
+++ controllers/listings/show.php
@@ -4,7 +4,7 @@
 $config = require basePath("config/myDB.php");
 $db = new Database($config);
 
-//get the id from the url by using the $_GET superglobal that is available globally
+//get the id from the url by using the $_GET superglobal that is available to the entire app
 $id = $_GET['id'] ?? '';
 
 //makes it safer to prevent unexpected sql injections in the database eg. accidentally dropping a table
@@ -12,7 +12,7 @@ $params = [
     'id' => $id
 ];
 
-//get the listing that matches the specific $id
+//get the listing that matches the specific $id 
 $listing = $db->query('SELECT * FROM listings WHERE id = :id', $params)->fetch();
 
 loadView('listings/show', [
