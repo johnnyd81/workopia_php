b48a56b4eef15cd1377e7c26f099f9658e5f37db
Jonathan Johnson <95863021+johnnyd81@users.noreply.github.com>
2025-07-18 11:11:31 +0200

    Update helpers.php

--- helpers.php
+++ helpers.php
@@ -7,7 +7,7 @@ Get the base path for a specific file
 */
 function basePath($path = '')
 {
-    //return the path as a string that can be required into a file
+    //return the path as a string that can be required into a specific file
     return __DIR__ . '/' . $path;
 }
 
