bbaaa6fd719c9635f770fe673fb3f3becbbfc68e
Jonathan Johnson <95863021+johnnyd81@users.noreply.github.com>
2025-08-09 20:23:35 +0200

    Update home.php

--- controllers/home.php
+++ controllers/home.php
@@ -6,7 +6,7 @@ $config = require basePath('config/myDB.php');
 //create a new database instance by using the Database class and using the $config array as the argument to the method
 $db = new Database($config);
 
-//fetch the listings in the database but limit them to 6 results at a time
+//fetches the listings in the database but limits them to 6 results at a time
 $listings = $db->query('SELECT * FROM listings LIMIT 6')->fetchAll();
 
 //loads the homepage using the custom-built loadView helper method. The available listings that were retrieved are passed in as the second argument
