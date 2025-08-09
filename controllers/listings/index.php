9c71a682f019c92acdbee86a04ab9bbe10441754
Jonathan Johnson <95863021+johnnyd81@users.noreply.github.com>
2025-08-09 20:24:27 +0200

    Update index.php

--- controllers/listings/index.php
+++ controllers/listings/index.php
@@ -7,7 +7,7 @@ $db = new Database($config);
 //store the available listings in the listings variable but limit them to 6 listings at each time
 $listings = $db->query('SELECT * FROM listings LIMIT 6')->fetchAll();
 
-//pass the retrieved listings into the view (i.e. the displayed webpage) to be displayed by using the custom-built loadView helper method
+//passes the retrieved listings into the view (i.e. the displayed webpage) to be displayed by using the custom-built loadView helper method
 loadView('listings/index', [
     'listings' => $listings
 ]);
