2cc4848441c58bfa100d8aef3b0d7159242941e7
Jonathan Johnson <95863021+johnnyd81@users.noreply.github.com>
2025-08-13 13:44:10 +0200

    Update create.php

--- controllers/listings/create.php
+++ controllers/listings/create.php
@@ -1,5 +1,5 @@
 <?php
 
-//displays the create listing form page that creates a new job listing
+//displays the create-listing form page that creates a new job listing
 //the custom-built loadView method is a helper method that was created to display data on the webpage
 loadView('/listings/create');
