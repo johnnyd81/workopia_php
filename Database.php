00ae4c054e1d08ca326aa04b36b66726cddd2a87
Jonathan Johnson <95863021+johnnyd81@users.noreply.github.com>
2025-08-28 09:06:38 +0200

    Update Database.php

--- Database.php
+++ Database.php
@@ -2,7 +2,7 @@
 
 class Database
 {
-    // public property to create a connection to the custom-built database i.e. workopia database
+    // public property to create a connection to the custom-built database for the app i.e. workopia database
     public $conn;
 
     /**
