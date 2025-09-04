9e9d1d1d861589048b386405d0ec9911bc9cce88
Jonathan Johnson <95863021+johnnyd81@users.noreply.github.com>
2025-09-04 10:08:30 +0200

    Update index.php

--- controllers/listings/index.php
+++ controllers/listings/index.php
@@ -4,7 +4,7 @@
 $config = require basePath("config/myDB.php");
 $db = new Database($config); //create a new database instance
 
-//store the retrieved listings in the listings variable but limit them to 6 listings at each time
+//the $listings variable stores the retrieved listings in the listings variable but limit them to 6 listings at each time
 $listings = $db->query('SELECT * FROM listings LIMIT 6')->fetchAll();
 
 //the method below passes the retrieved job listings into the view (i.e. the displayed webpage)
