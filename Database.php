5d7e59b4441513e186a7e82aa856ad6c57581337
Jonathan Johnson <95863021+johnnyd81@users.noreply.github.com>
2025-09-08 20:09:12 +0200

    Update Database.php

--- Database.php
+++ Database.php
@@ -6,7 +6,7 @@ class Database
     public $conn;
 
     /**
-     * Constructor to connect to my database 
+     * Constructor to connect to my custom database 
      * 
      * @param array $config
      * @return void
