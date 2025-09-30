232166b9737203b97eb43168c1de1d58a9e302a5
Jonathan Johnson <95863021+johnnyd81@users.noreply.github.com>
2025-09-30 09:57:15 +0200

    Update routes.php

--- routes.php
+++ routes.php
@@ -1,5 +1,5 @@
 <?php
-//all the required routes for the application
+//all the required routes for the app
 $router->get('/', 'controllers/home.php');
 $router->get('/listings', 'controllers/listings/index.php');
 $router->get('/listings/create', 'controllers/listings/create.php');
