2f298679b499a35f0ac8f1774b06fa126553c9a8
Jonathan Johnson <95863021+johnnyd81@users.noreply.github.com>
2025-10-12 12:36:46 +0200

    Update home.php

--- controllers/home.php
+++ controllers/home.php
@@ -9,7 +9,7 @@ $db = new Database($config);
 //gets all the available listings stored in the custom workopia database but limits them to 6 results at each time
 $listings = $db->query('SELECT * FROM listings LIMIT 6')->fetchAll();
 
-//the code below loads the homepage using the custom-built loadView helper method. The available listings that were fetched are passed in as the second argument
+//the code below loads the homepage using the custom-built loadView helper method. The available listings that are fetched are passed in as the second argument
 loadView('home', [
     'listings' => $listings
 ]);
