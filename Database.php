aa36d4fcb2202afcf4e8e12b3781ceba451c1f38
Jonathan Johnson <95863021+johnnyd81@users.noreply.github.com>
2025-10-20 10:09:41 +0200

    Update Database.php

--- Database.php
+++ Database.php
@@ -26,7 +26,7 @@ class Database
             //Create a new PDO instance to interact with the database i.e. the workopia database
             $this->conn = new PDO($dsn, $config['username'], $config['password'], $options);
         } catch (PDOException $e) {
-            // handles an exception if an error occurs while connecting to the database
+            // handles an exception if an error occurs while connecting to the local database
             //the method $e->getMessage() returns the error message
             throw new Exception("Database connection failed: {$e->getMessage()}");
         }
