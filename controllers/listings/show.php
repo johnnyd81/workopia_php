365ede0ae0e271f3f4e863af2d89456b7d7e39ed
Jonathan Johnson <95863021+johnnyd81@users.noreply.github.com>
2025-10-22 11:35:32 +0200

    Update show.php

--- controllers/listings/show.php
+++ controllers/listings/show.php
@@ -15,7 +15,7 @@ $params = [
 //return the correct job listing that matches the id 
 $listing = $db->query('SELECT * FROM listings WHERE id = :id', $params)->fetch();
 
-//load the user's view (webpage) using the custom-built loadView method stored in the helpers.php file
+//load the user's view (browser webpage) using the custom-built loadView method stored in the helpers.php file
 loadView('listings/show', [
     'listing' => $listing
 ]);
