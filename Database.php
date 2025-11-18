0430325f2132da9c9fccf6a552d4e7677af89b34
Jonathan Johnson <95863021+johnnyd81@users.noreply.github.com>
2025-11-18 20:14:37 +0200

    Update Database.php

--- Database.php
+++ Database.php
@@ -55,7 +55,7 @@ class Database
             $sth->execute();
             return $sth;
         } catch (PDOException $e) {
-            //an Exception will be thrown if an error occurs at any point during execution
+            //an Exception will be thrown if an error occurs at any point during the execution
             throw new Exception("Query failed to execute: {$e->getMessage()}");
         }
     }
