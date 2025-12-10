280b1788043a65025c2e1303a6b1fd948c1cf6ad
Jonathan Johnson <95863021+johnnyd81@users.noreply.github.com>
2025-12-10 13:18:11 +0200

    Update Router.php

--- Router.php
+++ Router.php
@@ -6,7 +6,7 @@ class Router
     //by adding it as a property the $routes array can be used anywhere in the Router class 
     protected $routes = [];
 
-    //the docblock below defines what a function does, the parameters it needs and what it returns when it is called i.e. being invoked
+    //the docblock below defines what the function does, the parameters it needs and what it returns when it is called i.e. being invoked
     /**
      * Add a new route
      *
