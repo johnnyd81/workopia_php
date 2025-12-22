e65ac97fa87d2b19b841d340f40c64e2ec6cdc76
Jonathan Johnson <95863021+johnnyd81@users.noreply.github.com>
2025-12-22 10:59:07 +0200

    Update show.php

--- controllers/listings/show.php
+++ controllers/listings/show.php
@@ -1,6 +1,6 @@
 <?php
 
-//require the myDB.php file to get the configuration details to connect to the custom made local workopia database
+//require the myDB.php file to get the configuration details to connect to the custom built local workopia database
 $config = require basePath("config/myDB.php");
 $db = new Database($config);
 
