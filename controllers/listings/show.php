e7d4301a488cd9427b66eecd54bddfe9e97e8c79
Jonathan Johnson <95863021+johnnyd81@users.noreply.github.com>
2025-12-22 11:00:46 +0200

    Update show.php

--- controllers/listings/show.php
+++ controllers/listings/show.php
@@ -2,7 +2,7 @@
 
 //require the myDB.php file to get the configuration details to connect to the custom built local workopia database
 $config = require basePath("config/myDB.php");
-$db = new Database($config);
+$db = new Database($config); //create a new Database instance
 
 //get the unique id from the browser url by using the $_GET superglobal that is available to the entire workopia app
 $id = $_GET['id'] ?? '';
