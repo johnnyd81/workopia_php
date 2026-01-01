05ab832a297a11cf8c3f342b2db9d8676c32fb84
Jonathan Johnson <95863021+johnnyd81@users.noreply.github.com>
2026-01-01 20:17:05 +0200

    Update helpers.php

--- helpers.php
+++ helpers.php
@@ -12,7 +12,7 @@ function basePath($path = '')
 }
 
 /*
-Load a page view i.e. homepage, listings, form etc.
+Load a page view i.e. homepage, listings, listing form etc.
 
 @param string $name
 $param array $data
