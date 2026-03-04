35f50267c9d0d6fc24204e3a03ace1d513fcd26d
Jonathan Johnson <95863021+johnnyd81@users.noreply.github.com>
2026-03-04 10:29:52 +0200

    Update show.php

--- controllers/listings/show.php
+++ controllers/listings/show.php
@@ -15,7 +15,7 @@ $params = [
 //return the correct job listing that matches the unique id for the user
 $listing = $db->query('SELECT * FROM listings WHERE id = :id', $params)->fetch();
 
-//load the user's view (browser webpage) using the custom-made loadView method stored in the helpers.php file in the root of the app
+//load the user's view (user's webpage) using the custom-made loadView method stored in the helpers.php file in the root of the app
 loadView('listings/show', [
     'listing' => $listing
 ]);
