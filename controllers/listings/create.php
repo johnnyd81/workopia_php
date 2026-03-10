22d08f2d558f73a87cf0f3b5c749650cd7532ea3
Jonathan Johnson <95863021+johnnyd81@users.noreply.github.com>
2026-03-10 11:32:32 +0200

    Update create.php

--- controllers/listings/create.php
+++ controllers/listings/create.php
@@ -1,5 +1,5 @@
 <?php
 
-//this file shows the job listing form that creates a brand new job listing to add to the custom-built local workopia database
+//this file shows the job listing form that creates a brand new job listing to add to the custom-made local workopia database
 //the custom-built loadView method below is a helper method that was created to display custom data on the user browser's webpage
 loadView('/listings/create');
