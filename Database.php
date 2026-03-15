a0a0c1efb323260f20d4af305e6a517826d66612
Jonathan Johnson <95863021+johnnyd81@users.noreply.github.com>
2026-03-15 14:09:26 +0200

    Update Database.php

--- Database.php
+++ Database.php
@@ -43,7 +43,7 @@ class Database
     public function query($query, $params = [])
     {
         try {
-           //create a statement variable i.e. $sth that will contain the query data (job listings) from the database when it is retrieved successfully
+           //create a statement variable i.e. $sth that will contain the query data (available job listings) from the database when it is retrieved successfully
             $sth = $this->conn->prepare($query);
 
             //Bind the named params in the query statement
