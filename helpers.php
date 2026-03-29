2c7407c7507d4f5d1a955df0f1af9a8636c670b1
Jonathan Johnson <95863021+johnnyd81@users.noreply.github.com>
2026-03-29 14:17:57 +0200

    Update helpers.php

--- helpers.php
+++ helpers.php
@@ -30,7 +30,7 @@ function loadView($name, $data = [])
         extract($data);
         require $viewPath;
     } else {
-        //print a message for the user if a specific view cannot be found
+        //print a message for the user if a specific view isn't available
         echo "View {$viewPath} not found!";
     }
 }
