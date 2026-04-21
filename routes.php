98ccb528f72856fbaaf4014ee16a69b89721c5e9
Jonathan Johnson <95863021+johnnyd81@users.noreply.github.com>
2026-04-21 15:10:57 +0200

    Update routes.php

--- routes.php
+++ routes.php
@@ -1,5 +1,5 @@
 <?php
-//all the GET routes to be used in the workopia job listing application i.e. root route, create route, job listings route etc.
+//all the GET routes to be used in the workopia job listing app i.e. root route, create route, job listings route etc.
 $router->get('/', 'controllers/home.php');
 $router->get('/listings', 'controllers/listings/index.php');
 $router->get('/listings/create', 'controllers/listings/create.php');
