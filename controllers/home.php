1e13cd116cdf78dbf53527c5e9964850ca10a6d2
Jonathan Johnson <95863021+johnnyd81@users.noreply.github.com>
2026-05-28 09:30:17 +0200

    Update home.php

--- controllers/home.php
+++ controllers/home.php
@@ -6,7 +6,7 @@ $config = require basePath('config/myDB.php');//the custom-built basePath method
 //create a new database instance by using the custom Database class and using the imported $config array as the only argument to the custom-made Database class
 $db = new Database($config);
 
-//gets all the available job listings stored in the custom-made local workopia database but limits them to 6 results per time
+//gets all the available job listings stored in the custom-made local workopia database but limits them to 6 results per request
 $listings = $db->query('SELECT * FROM listings LIMIT 6')->fetchAll();
 
 //the code below shows the main homepage using the custom-made loadView helper method. The available database listings are then used in the method
