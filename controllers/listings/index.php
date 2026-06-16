b67597e59321685f14c00aab345994e640292ab9
Jonathan Johnson <95863021+johnnyd81@users.noreply.github.com>
2026-06-16 10:54:07 +0200

    Update index.php

--- controllers/listings/index.php
+++ controllers/listings/index.php
@@ -3,7 +3,7 @@
 //import the myDB.php file that returns the configuration data to connect to the custom-built local workopia database
 $db = new Database($config); //this creates a new database instance with the custom-made Database class
 
-//the $listings variable contains the retrieved available job listings found in the custom-made workopia database but limits the listings to 6 at a time
+//the $listings variable contains the retrieved available job listings left in the custom-made workopia database but limits the listings to 6 at a time
 $listings = $db->query('SELECT * FROM listings LIMIT 6')->fetchAll();
 
 //the loadView method shown below shows the retrieved job listings that are stored in the custom-made database onto the user's webpage
