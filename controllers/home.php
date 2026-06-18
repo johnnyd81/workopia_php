a5d486abb5acac527526a4c11220c1f64605fd73
Jonathan Johnson <95863021+johnnyd81@users.noreply.github.com>
2026-06-18 10:21:15 +0200

    Update home.php

--- controllers/home.php
+++ controllers/home.php
@@ -9,7 +9,7 @@ $db = new Database($config);
 //returns all the available job listings stored in the custom-made local workopia database but limits them to 6 results per request
 $listings = $db->query('SELECT * FROM listings LIMIT 6')->fetchAll();
 
-//the code below shows the main homepage using the custom-made loadView helper method. The available database job-listings are then used in the method
+//the code below shows the main homepage using the custom-made loadView helper method. The available database job-listings are then added to the method
 loadView('home', [
     'listings' => $listings
 ]);
