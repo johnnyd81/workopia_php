5b3165433ea990971c3cafeed8a2413eb5953484
Jonathan Johnson <95863021+johnnyd81@users.noreply.github.com>
2026-06-22 10:38:01 +0200

    Update 404.php

--- controllers/error/404.php
+++ controllers/error/404.php
@@ -1,5 +1,5 @@
 <?php
 
 //this file shows the 404 error page using the custom-made loadView helper method found in the helpers.php file in the root of the app
-//the argument in the loadView method shown below must be formatted as a string i.e. characters and numbers placed between single or double quotes
+//the argument in the loadView method shown below must be formatted as a string i.e. characters, numbers and symbols placed between single or double quotes
 loadView('error/404');
