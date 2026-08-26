8994adf268fa59e098b75cd6ebdaa1d5826e9005
Jonathan Johnson <95863021+johnnyd81@users.noreply.github.com>
2026-08-26 09:58:23 +0200

    Update helpers.php

--- helpers.php
+++ helpers.php
@@ -36,7 +36,7 @@ function loadView($name, $data = [])
 }
 
 /*
-Load a ui partial i.e. a ui component like a navbar, footer etc.
+Load a ui partial i.e. a ui component like a navbar, footer etc
 
 @param string $name
 @return void
