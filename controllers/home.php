a6e8b590095ac499a3ce916a94d9e8d22406d667
Jonathan Johnson <95863021+johnnyd81@users.noreply.github.com>
2026-09-23 06:59:26 +0200

    Update home.php

--- controllers/home.php
+++ controllers/home.php
@@ -1,6 +1,6 @@
 <?php
 
-//import the myDB.php file that has the required configuration data to connect to the custom-made local workopia database for the application
+//import the myDB.php file that has the required configuration data to connect to the custom-designed local workopia database for the application
 $config = require basePath('config/myDB.php');//the custom-designed basePath method from the app's helpers.php file
 
 //create a new database instance by using the custom Database class and using the imported $config array as the only argument to the custom-made Database class
